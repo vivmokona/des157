a21af48e89c3a29ac159ce6aca3aa91af7bcbf74
Vivian <vyluu@ucdavis.edu>
2018-01-30 16:06:14 -0800

    Studio 1
    Draft 5

--- studio1/script.js
+++ studio1/script.js
@@ -8,6 +8,13 @@ document.f.onsubmit = processForm;
 //captures the reset event
 //document.f.onreset = resetForm;
 
+/*set the hide the message and the image
+function resetForm() {
+    myMsg.innerHTML = "";
+    myMsg.setAttribute('class', 'hide');
+    results.setAttribute('class', 'hide');
+*/
+
 
 //intialize to get text input
 var results = document.getElementById("results");
@@ -20,7 +27,7 @@ function processForm() {
     results.setAttribute('class', 'show');
 
     //store fields in variables
-   // var userName = document.f.userName.value;
+    // var userName = document.f.userName.value;
     var adj = document.f.adj.value;
     var noun1 = document.f.noun1.value;
     var tea = document.f.tea.value;
@@ -28,25 +35,17 @@ function processForm() {
     var num2 = document.f.num2.value;
     var verb = document.f.verb.value;
     var noun2 = document.f.noun2.value;
-    
+
     document.getElementById("myMsg").innerHTML;
 
     // My message
-    myMsg.innerHTML = "Boil," + tea + " leaves <br>"
-        +"Add" + num1 + "% sugar  <br>"
-        +"Add" + num2 + "% ice <br>"
-        +"Shake and " + verb + "around! <br>"
-        +"Add" + noun2 + " and boba <br>"
-        +"Serve and sip on your " + adj + noun1;
+    myMsg.innerHTML = "Boil " + tea + " tea leaves <br>" +
+        "Add " + num1 + "% sugar  <br>" +
+        "Add " + num2 + "% ice <br>" +
+        "Shake and " + verb + " around! <br>" +
+        "Add " + noun2 + " and boba <br>" +
+        "Serve and sip on your " + adj + noun1;
 
     //prevent page from reloading
     return false;
 }
-
- /*set the hide the message and the image
-function resetForm() {
-    myMsg.innerHTML = "";
-    myMsg.setAttribute('class', 'hide');
-    results.setAttribute('class', 'hide');
-
-} */
